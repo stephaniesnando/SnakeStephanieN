1ea0cbcef0e91f90feeb6188024d71dced4ccacb
Stephanie Nando <stephaniesnando@gmail.com>
2014-10-04 15:06:18 -0700

    Day 3

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,11 +1,14 @@
 var snake;
+var snakeLength;
+var snakeSize;
 
 var context;
 var screenWidth;
 var screenHeight;
 
-gameDraw();
 gameInitialize();
+snakeInitialize();
+setInterval(gameLoop, 1000/30);
 
 function gameInitialize() {
   var canvas = document.getElementById("game-screen");
@@ -19,10 +22,44 @@ function gameInitialize() {
   }
   
 function gameLoop() {
-    
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
 }
 
 function gameDraw() {
-    context.fillStyle = "rgb(245, 196, 214)";
+    context.fillStyle = "rgb(181, 175, 240)";
     context.fillRect(0, 0, screenWidth, screenHeight);    
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for(var index = snakeLength - 1; index >= 0; index--) {
+        snake.push( {
+           x: index, 
+           y: 0
+        });
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "white";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
 }
