6ccd7fdb9a656bfb7343334e60851f39e89aabbe
Stephanie Nando <stephaniesnando@gmail.com>
2014-10-22 11:25:35 -0700

    Day 7

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,3 +1,5 @@
+/*-- Variables --*/
+
 var snake;
 var snakeLength;
 var snakeSize;
@@ -9,10 +11,14 @@ var context;
 var screenWidth;
 var screenHeight;
 
+/* Executing Game Code -----*/
+
 gameInitialize();
 snakeInitialize();
 foodInitialize();
 setInterval(gameLoop, 1000/30);
+
+/*-------- Game functions ---------*/
 
 function gameInitialize() {
   var canvas = document.getElementById("game-screen");
@@ -67,15 +73,25 @@ function snakeUpdate() {
     if(snakeDirection == "down") {
         snakeHeadY++;
     }
-    else {
+    else if(snakeDirection == "right") {
         snakeHeadX++;
     }
+    else if(snakeDirection == "left") {
+        snakeHeadX--;
+    }
+    else if(snakeDirection == "up") {
+        snakeHeadY--;
+    }
+    
+    checkFoodCollisions();
     
     var snakeTail = snake.pop();
     snakeTail.x = snakeHeadX;
     snakeTail.y = snakeHeadY;
     snake.unshift(snakeTail);
 }
+
+/*------ Food FUnctions ----*/
 
 function foodInitialize() {
     food = {
@@ -87,25 +103,41 @@ function foodInitialize() {
 
 function foodDraw() {
     context.fillStyle = "black";
-    context.fillRect(food.x, food.y, snakeSize,snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize,snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random () * screenWidth);
     var randomY = Math.floor(Math.random () * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor (randomX / snakeSize);
+    food.y = Math.floor (randomY / snakeSize);
 }
 
-/*--------------------------------------------------------------------
+/*-------------------------------
  * Input Functions
  */
 
 function keyboardHandler (event) {
     console.log(event);
     
-    if(event.keyCode == "39") {
+    if(event.keyCode == "39" && snakeDirection != "left") {
         snakeDirection = "right";
+    }
+    else if(event.keyCode == "40" && snakeDirection != "up") {
+        snakeDirection = "down";
+    }
+    else if (event.keyCode == "37" && snakeDirection != "right") {
+        snakeDirection = "left";
+    }
+    else if (event.keyCode == "38" && snakeDirection != "down") {
+        snakeDirection = "up";
+    }
+}
+/*-- collision handling --*/
+
+function checkFoodCollisions () {
+    if () {
+        
     }
 }
